73a93dfe3e433f52fd698e2912abad97f3d6f2ab
Ania-su <antsaaa.randria@gmail.com>
2025-09-03 09:39:25 +0300

    create Fast API endpoints

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from starlette.responses import Response
 
 app = FastAPI()
 
-@app.get("/ping")
-def hello():
-    return Response(content="pong", status_code=200, media_type="text/plain")
+@app.get("/health")
+def get_health():
+    return Response(content="Ok", status_code=200, media_type="text/plain")
+
+class Characteristic(BaseModel):
+    ram_memory: int
+    rom_memory: int
+
+class Phone(BaseModel):
+    identifier : int
+    brand : str
+    model : str
+    characteristics : Characteristic
+
+phones : List[Phone] = []
+
+@app.post("/phones")
+def create_phone(new_phone: List[Phone]):
+    phones.extend(new_phone)
+    return phones
+
+@app.get("/phones")
+def get_phones():
+    return phones
+
+@app.get("/phones/{phone_id}")
+def get_phone(phone_id: int):
+    if phone_id not in phones:
+        raise HTTPException(status_code=404, detail="Phone not found")
+    return phones[phone_id]
